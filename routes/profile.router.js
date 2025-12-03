3e4c9dd461ff3aa7476de44f94aaed74ab88909c
Alejandro Di Stefano <addistefano@live.com.ar>
2025-12-03 11:44:29 -0300

    Clase 02

--- routes/profile.router.js
+++ routes/profile.router.js
@@ -5,9 +5,13 @@ import { requireLogin } from '../middleware/auth.middleware.js';
 const router = Router();
 
 router.get('/', requireLogin, (req, res) => {
-    const { first_name, last_name, email, age } = req.session.user;
-    const bienvenida = `Bienvenido ${first_name} ${last_name}.!`
-    res.status(200).json({ message: bienvenida, user: { first_name, last_name, email, age } });
+    try {
+        const { first_name, last_name, email, age } = req.session.user;
+        const bienvenida = `Bienvenido ${first_name} ${last_name}.!`
+        res.status(200).json({ message: bienvenida, user: { first_name, last_name, email, age } });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
 export default router;
